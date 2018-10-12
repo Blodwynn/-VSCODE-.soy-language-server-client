5e083dcd55d0495c12dfce47bd644df6f89d5c8d
Roland Orosz <orrsaai@gmail.com>
2018-10-12 15:48:58 +0200

    refactor

--- client/src/soy-definition-provider.ts
+++ client/src/soy-definition-provider.ts
@@ -1,46 +1,11 @@
 import vscode = require('vscode');
 import glob = require('glob');
 import path = require('path');
-import { SoyDefinitionInformation, TemplatePathMap, TemplatePathDescription } from './interfaces';
+import { SoyDefinitionInformation, TemplatePathMap } from './interfaces';
 import { parseFiles } from './parse';
-
-function normalizeAliasTemplate(alias: string, template: string): string {
-    const truncatedAliasPath: string = alias.substr(0, alias.lastIndexOf('.') + 1);
-
-    return `${truncatedAliasPath}${template}`;
-}
-
-function getNamespace(documentText: string): string {
-    const namespacePattern: RegExp = /\{namespace\s*([\w\d.]+)/;
-    const namespaceMatch = namespacePattern.exec(documentText);
-
-    if (namespaceMatch) {
-        return namespaceMatch[1];
-    }
-
-    return null;
-}
-
-function getMatchingAlias(template: string, aliases: string[]): string {
-    const matchingPart = template.split('.')[0];
-
-    return aliases.find(alias => alias.endsWith(matchingPart));
-}
-
-function getAliases(documentText: string): string[] {
-    const aliasPattern: RegExp = /\{alias\s*([\w\d.]+)/gm;
-    let aliases: string[] = [];
-    let m;
-
-    while (m = aliasPattern.exec(documentText)) {
-        aliases.push(m[1]);
-    }
-
-    return aliases;
-}
+import { getTemplateDescription } from './template';
 
 function getSoyFiles() {
-    // const currentWorkspaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath;
     let fileList: string[] = [];
 
     vscode.workspace.workspaceFolders.forEach(wsFolder => {
@@ -56,28 +21,6 @@ function getSoyFiles() {
     });
 
     return fileList;
-}
-
-function getTemplateDescription(templateToSearchFor: string, templatePathMap: TemplatePathMap, document: vscode.TextDocument): TemplatePathDescription {
-    const documentText: string = document.getText();
-    const namespace: string = getNamespace(documentText);
-    const aliases: string[] = getAliases(documentText);
-    let templateData: TemplatePathDescription;
-
-    if (templateToSearchFor.startsWith('.')) {
-        const templateNamespace = `${namespace}${templateToSearchFor}`;
-        templateData = templatePathMap[templateNamespace];
-    } else {
-        templateData = templatePathMap[templateToSearchFor];
-
-        if (!templateData || !templateData.path) {
-            const alias: string = getMatchingAlias(templateToSearchFor, aliases);
-            const fullTemplatePath: string = normalizeAliasTemplate(alias, templateToSearchFor);
-            templateData = templatePathMap[fullTemplatePath];
-        }
-    }
-
-    return templateData;
 }
 
 export function definitionLocation(document: vscode.TextDocument, position: vscode.Position, templatePathMap: TemplatePathMap): Promise<SoyDefinitionInformation> {
@@ -105,8 +48,8 @@ export function definitionLocation(document: vscode.TextDocument, position: vsco
 }
 
 export class SoyDefinitionProvider implements vscode.DefinitionProvider {
-    files: any;
-    templatePathMap: any;
+    files: string[];
+    templatePathMap: TemplatePathMap;
 
     constructor() {
         this.files = getSoyFiles();
