f4cf17aae3eb79ee6c9ef944c4f06807351677fb
Roland Orosz <orrsaai@gmail.com>
2018-10-31 08:21:40 +0100

    minor refactor

--- client/src/reference-provider/parse.ts
+++ client/src/reference-provider/parse.ts
@@ -2,16 +2,29 @@ import linenumber = require('linenumber');
 import fs = require('fs');
 import { getNamespace, getAliases, getMatchingAlias, normalizeAliasTemplate } from '../utils';
 
-function escapeRegExp(string) {
+function escapeRegExp (string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-function insertElementWithKey(templateName: string, file: any, allCallMaps: any) {
+function insertElementWithKey (templateName: string, file: any, allCallMaps: any) {
     if (Array.isArray(allCallMaps[templateName])) {
         allCallMaps[templateName].push(file);
     } else {
         allCallMaps[templateName] = new Array(file);
     }
+}
+
+function insertCalls (templateName: string, file: any, lineNrs: any, allCallMaps: any) {
+    lineNrs.forEach(lineItem => {
+        insertElementWithKey(
+            templateName,
+            {
+                file,
+                line: lineItem.line - 1
+            },
+            allCallMaps
+        );
+    });
 }
 
 function parseFile (file, allCallMaps) {
@@ -25,49 +38,19 @@ function parseFile (file, allCallMaps) {
         const template = m[1];
 
         if (template.startsWith('.')) {
-            lineNr.forEach(lineItem => {
-                insertElementWithKey(
-                    `${namespace}${template}`,
-                    {
-                        file,
-                        line: lineItem.line - 1
-                    },
-                    allCallMaps
-                );
-
-            });
+            insertCalls(`${namespace}${template}`, file, lineNr, allCallMaps);
         } else {
             const aliases: string[] = getAliases(content);
             const alias: string = getMatchingAlias(template, aliases);
 
             if (alias) {
                 const fullTemplatePath: string = normalizeAliasTemplate(alias, template);
-
-                lineNr.forEach(lineItem => {
-                    insertElementWithKey(
-                        fullTemplatePath,
-                        {
-                            file,
-                            line: lineItem.line - 1
-                        },
-                        allCallMaps
-                    );
-                });
+                insertCalls(fullTemplatePath, file, lineNr, allCallMaps);
             } else {
-                lineNr.forEach(lineItem => {
-                    insertElementWithKey(
-                        template,
-                        {
-                            file,
-                            line: lineItem.line - 1
-                        },
-                        allCallMaps
-                    );
-                });
+                insertCalls(template, file, lineNr, allCallMaps);
             }
         }
     }
-
 }
 
 export function parseFilesForReferences (wsFolders) {
