624fbaec90aa60a77af9fdd2dd3a04c0baf96de6
Roland Orosz <orrsaai@gmail.com>
2018-11-26 14:19:01 +0100

    adding outliner

--- client/src/document-symbol-provider/soy-document-symbol-provider.ts
+++ client/src/document-symbol-provider/soy-document-symbol-provider.ts
@@ -1,24 +1,45 @@
-import { DocumentSymbolProvider, TextDocument, ProviderResult, SymbolInformation } from 'vscode';
+import { DocumentSymbolProvider, TextDocument, ProviderResult, SymbolInformation, SymbolKind, Location } from 'vscode';
 
 export class SoyDocumentSymbolProvider implements DocumentSymbolProvider {
 
     public provideDocumentSymbols (document: TextDocument): ProviderResult<SymbolInformation[]> {
         const matches = this.getSymbolData(document.getText());
+        let containerName: string = '';
 
-        console.log(matches);
+        return matches.map(match => {
+            const type = match[1];
+            const name = match[2];
+            const kind = this.getTokenSymbolKind(type);
+            const position = document.positionAt(match.index || 0);
 
-        return null;
+            if (kind === SymbolKind.Class) {
+                containerName = name;
+            }
+
+            return new SymbolInformation(name, kind, containerName, new Location(document.uri, position));
+        });
     }
 
     private getSymbolData (documentText: string): RegExpMatchArray[] {
-        const pattern: RegExp = /(?:del)?(?:template|call)\s+([\w\d.]+)/gm;
+        const pattern: RegExp = /((?:del)?(?:template|call))\s+([\w\d.]+)/gim;
         const matches: RegExpMatchArray[] = [];
-        let m: RegExpMatchArray = pattern.exec(documentText);
+        let m: RegExpMatchArray;
 
         while (m = pattern.exec(documentText)) {
             matches.push(m);
         }
 
         return matches;
+    }
+
+    private getTokenSymbolKind (token: string): SymbolKind {
+        const symbolTokenMap = {
+            'template': SymbolKind.Class,
+            'deltemplate': SymbolKind.Class,
+            'call': SymbolKind.Function,
+            'delcall': SymbolKind.Function
+        };
+
+        return symbolTokenMap[token];
     }
 }
